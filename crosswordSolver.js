627cf3aabfe1e25f4bea544743b6f547e6905e42
helouzizi <ouazizi2code@gmail.com>
2025-02-24 13:48:49 +0000

    add some code

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,14 +1,49 @@
-function crosswordSolver(emptypuzll,words){
-    if (check_empty_puzzle(emptypuzll)){
+const puzzle = `2001
+0..0
+1000
+0..0`
+
+const words =['casa', 'alan', 'ciao', 'anta']
+
+
+function crosswordSolver(emptypuzll, words) {
+    // lets check for inputs puzzle and words rules
+    if (check_inputs(emptypuzll,words)) {
         return 'Error'
     }
-   
+    let puzzle_matrix = split(emptypuzll)
+    console.log(emptypuzll)
+    console.log(words)
+
     return "puzzle is safe"
 }
 
-function check_empty_puzzle(emptypuzll){
+function check_inputs(emptypuzll, words) {
     let regex = /^[0-9.\n]+$/g
-    return regex.test(emptypuzll) ? false : true
+    let uniqwords = new Set()
+    for (let word of words) {
+        if (uniqwords.has(word)) {
+          return true
+        }
+        uniqwords.add(word)
+    }
+    return !regex.test(emptypuzll)
 }
 
-console.log(crosswordSolver("000021555.\n"))
+function split(puzzl){
+    return puzzl.split('\n').map(row => row.split(''))
+}
+
+function findWordsPositions(puzlle_matrix){
+    let positions = []
+
+    for (let i = 0 ; i< puzlle_matrix.length; i++){
+        for (let j = 0; j < puzlle_matrix[i].length; j++){
+            
+        }
+    }
+
+
+}
+console.log(words.map(test => test.split('')))
+console.log(split(puzzle))
